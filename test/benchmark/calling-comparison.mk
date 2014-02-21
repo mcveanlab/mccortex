3841f6b218a36927e4b032585d97b33aaed79312
Isaac Turner <turner.isaac@gmail.com>
2014-02-21 07:25:20 +0000

    Fix bash that I keep messin gup

--- test/benchmark/calling-comparison.mk
+++ test/benchmark/calling-comparison.mk
@@ -46,7 +46,7 @@ STAMPY_BIN=./stampy.sh
 
 NUMCOLS=$(shell echo $$(($(NUM_INDIVS)+1)))
 # 8bytes for kmer, 4 for covgs, 1 for edges + 10MB for paths
-MEM=$(shell echo '$[ $(MEMWIDTH) * (2^$(MEMHEIGHT)) * (8+(4+1)*$(NUMCOLS)) + 10000000 ]')
+MEM=$(shell echo '$$[ $(MEMWIDTH) * (2^$(MEMHEIGHT)) * (8+(4+1)*$(NUMCOLS)) + 10000000 ]')
 
 RELEASECTX=$(CORTEX_PATH)/bin/cortex_var_31_c$(NUMCOLS) --kmer_size $(KMER) --mem_height $(MEMHEIGHT) --mem_width $(MEMWIDTH)
 CTX=$(CTX_PATH)/bin/ctx31
