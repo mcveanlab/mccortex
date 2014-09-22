mcveanlab/mccortex

commit 93ae0d7c85b31edbbf10262d3b8990757691fa29

correct_reads.c using indel+qual safe code; Minor bug fixes.
0x0. cmd.c clones input arguments array

0x1. JSON header: better reporting of command line args - use array of strings
     instead of merging into a string without escaping spaces.

0x2. Print STDOUT/STDIN instead of "-" in lots of places.

0x4. scripts/seq2pdf.sh: fix some bugs when running not in scripts dir

0x8. Don't try to get file size when reading from STDIN ("-")

0x10. objbuf_macro.h: added FUNC_shift() function for shifting elements

0x20. ctx_exp_abc.c: report GraphWalker state at the end of traversals

0x40. util.c: added function util_print_nums() to report numbers nicely aligned

0x80. correct_reads.c: lots of work on correcting reads. Now using new version of
      code. Indels still not printing correctly though. Just a bit of tweaking
      required. Soon will handle indels and quality scores nicely.

src/alignment/correct_alignment.c
@@ -506,7 +506,7 @@ void correct_aln_read(CorrectAlnWorker *wrkr, const CorrectAlnParam *params,
                       const read_t *r, uint8_t fq_cutoff, uint8_t hp_cutoff,
                       dBNodeBuffer *nodebuf, Int32Buffer *posbuf)
 {
-  size_t i, j, end_len;
+  size_t i, j;
 
   db_node_buf_reset(nodebuf);
   int32_buf_reset(posbuf);
@@ -528,64 +528,78 @@ void correct_aln_read(CorrectAlnWorker *wrkr, const CorrectAlnParam *params,
   }
 
   size_t left_gap = aln->rpos.data[0], right_gap = aln->r1enderr;
-  const dBNodeBuffer *nbuf;
 
   // Temporary walkers used to traverse the graph
   GraphWalker *wlk = &wrkr->wlk;
   RepeatWalker *rptwlk = &wrkr->rptwlk;
 
   // Get first alignment, if there are gaps, backtrack
-  nbuf = correct_alignment_nxt(wrkr);
-  ctx_assert(nbuf != NULL); // We've already checked aln->nodes.len > 0
-
-  if(left_gap > 0)
-  {
-    // Try to fill in missing kmers
-    graph_walker_prime(wlk, nbuf->data, nbuf->len,
-                       params->max_context, false,
-                       params->ctxcol, params->ctpcol,
-                       wrkr->db_graph);
-
-    db_node_buf_ensure_capacity(nodebuf, left_gap);
-    int32_buf_ensure_capacity(  posbuf,  left_gap);
-
-    for(i = 0; i < left_gap && graph_walker_next(wlk) &&
-               rpt_walker_attempt_traverse(rptwlk, wlk);  i++)
-    {
-      nodebuf->data[nodebuf->len] = wlk->node;
-      posbuf->data[nodebuf->len] = -1;
-    }
-    nodebuf->len = posbuf->len = i;
-
-    graph_walker_finish(wlk);
-    rpt_walker_fast_clear(rptwlk, nodebuf->data, nodebuf->len);
-
-    // Reverse lists, reverse nodes
-    for(i = 0, j = nodebuf->len-1; i < nodebuf->len; i++, j--) {
-      dBNode n = nodebuf->data[i];
-      nodebuf->data[i] = db_node_reverse(nodebuf->data[j]);
-      nodebuf->data[j] = db_node_reverse(n);
-      SWAP(posbuf->data[i], posbuf->data[j]);
-    }
-  }
+  // nbuf = correct_alignment_nxt(wrkr);
+  // ctx_assert(nbuf != NULL); // We've already checked aln->nodes.len > 0
 
   // Append contigs
-  while((nbuf = correct_alignment_nxt(wrkr)) != NULL)
+  while(correct_alignment_nxt(wrkr) != NULL)
   {
+    // printf("got thing!\n");
     ctx_assert(wrkr->contig.len ==  wrkr->rpos.len);
     db_node_buf_append(nodebuf, wrkr->contig.data, wrkr->contig.len);
     int32_buf_append(posbuf, wrkr->rpos.data, wrkr->rpos.len);
   }
 
+  // Couldn't get any kmers
+  if(nodebuf->len == 0) {
+    // printf("\nGot no kmers!\n\n");
+    return;
+  }
+
+  if(left_gap > 0)
+  {
+    dBNodeBuffer *revcontig = &wrkr->revcontig;
+    db_node_buf_reset(revcontig);
+    db_node_buf_ensure_capacity(revcontig, left_gap);
+
+    // Try to fill in missing kmers
+    size_t n = 1;
+    while(n < nodebuf->len && posbuf->data[n] == posbuf->data[n-1]+1) { n++; }
+
+    graph_walker_prime(wlk, nodebuf->data, n,
+                       params->max_context, false,
+                       params->ctxcol, params->ctpcol,
+                       wrkr->db_graph);
+
+    for(i = 0; i < left_gap && graph_walker_next(wlk) &&
+                               rpt_walker_attempt_traverse(rptwlk, wlk);  i++)
+    {
+      revcontig->data[i] = wlk->node;
+    }
+    revcontig->len = i;
+
+    graph_walker_finish(wlk);
+    rpt_walker_fast_clear(rptwlk, revcontig->data, revcontig->len);
+
+    int32_buf_shift(posbuf, revcontig->len);
+    db_node_buf_shift(nodebuf, revcontig->len);
+
+    // copy in reverse order
+    for(i = 0, j = revcontig->len-1; i < revcontig->len; i++, j--) {
+      nodebuf->data[i] = db_node_reverse(nodebuf->data[j]);
+      posbuf->data[i] = -1;
+    }
+  }
+
   if(right_gap > 0)
   {
     // Extend to the right
-    graph_walker_prime(wlk, nbuf->data, nbuf->len,
+    size_t n = nodebuf->len-1;
+    while(n > 0 && posbuf->data[n] == posbuf->data[n-1]+1) { n--; }
+
+    graph_walker_prime(wlk, nodebuf->data + n, nodebuf->len - n,
                        params->max_context, true,
                        params->ctxcol, params->ctpcol,
                        wrkr->db_graph);
 
-    end_len = nodebuf->len + right_gap;
+    size_t orig_len = nodebuf->len;
+    size_t end_len = nodebuf->len + right_gap;
     db_node_buf_ensure_capacity(nodebuf, end_len);
     int32_buf_ensure_capacity(  posbuf,  end_len);
 
@@ -598,6 +612,6 @@ void correct_aln_read(CorrectAlnWorker *wrkr, const CorrectAlnParam *params,
     nodebuf->len = posbuf->len = i;
 
     graph_walker_finish(wlk);
-    rpt_walker_fast_clear(rptwlk, nbuf->data, nbuf->len);
+    rpt_walker_fast_clear(rptwlk, nodebuf->data+orig_len, nodebuf->len-orig_len);
   }
 }

src/graph/graph_walker.h
@@ -24,6 +24,17 @@ typedef struct
 #define GRPHWLK_SPLIT_PATHS 5 /* Fail: fork in colour, paths split */
 #define GRPHWLK_MISSING_PATHS 6 /* Fail: fork in colour, missing info */
 #define GRPHWLK_USEPATH 7 /* Success: fork in colour, paths resolved */
+#define GRPHWLK_NUM_STATES 8
+
+#define GRPHWLK_FORWARD_STR       "GoForward"
+#define GRPHWLK_COLFWD_STR        "GoColForward"
+#define GRPHWLK_NOCOVG_STR        "FailNoCovg"
+#define GRPHWLK_NOCOLCOVG_STR     "FailNoColCovg"
+#define GRPHWLK_NOPATHS_STR       "FailNoPaths"
+#define GRPHWLK_SPLIT_PATHS_STR   "FailSplitPaths"
+#define GRPHWLK_MISSING_PATHS_STR "FailMissingPaths"
+#define GRPHWLK_USEPATH_STR       "GoUsePath"
+
 
 extern const char *graph_step_str[];
 
@@ -59,6 +70,7 @@ void graph_walker_alloc(GraphWalker *wlk);
 void graph_walker_dealloc(GraphWalker *gw);
 
 char* graph_walker_status2str(uint8_t status, char *str, size_t len);
+void graph_walker_print_state_hist(const size_t arr[GRPHWLK_NUM_STATES]);
 void graph_walker_print_state(const GraphWalker *wlk, FILE *fout);
 
 // Always call finish after calling init

scripts/seq2pdf.sh
@@ -6,7 +6,7 @@ set +o posix
 
 args=
 
-if [[ $1 == "--simplify" ]]
+if [[ ($# -gt 1) && ($1 == "--simplify") ]]
 then
   args=$1
   shift
@@ -33,7 +33,7 @@ fi
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CTX="$DIR/../bin/ctx$maxk"
-
+CTX2GRAPHVIZ="$DIR/cortex_to_graphviz.pl"
 if [[ !(-e $CTX) || !(-x $CTX) ]]
 then
   echo "Did you compile for MAXK=$maxk? I cannot run $CTX"
@@ -43,5 +43,5 @@ fi
 files=$(printf " --seq %s" $@; printf "\n")
 
 $CTX build -k $kmer --sample Test $files - | \
-  ./scripts/cortex_to_graphviz.pl -k $kmer $args - | \
+  $CTX2GRAPHVIZ -k $kmer $args - | \
   dot -Tpdf

src/alignment/correct_alignment.h
@@ -19,14 +19,17 @@
 
 #define DEFAULT_CRTALN_MAX_CONTEXT 200
 
-#define CORRECT_PARAMS_DEFAULT {.ctpcol = 0, .ctxcol = 0,                      \
-                                .frag_len_min = DEFAULT_CRTALN_FRAGLEN_MIN,    \
-                                .frag_len_max = DEFAULT_CRTALN_FRAGLEN_MAX,    \
-                                .one_way_gap_traverse = true,                  \
-                                .use_end_check = true,                         \
-                                .max_context = DEFAULT_CRTALN_MAX_CONTEXT,     \
-                                .gap_variance = DEFAULT_CRTALN_GAP_VARIANCE,   \
-                                .gap_wiggle = DEFAULT_CRTALN_GAP_WIGGLE}
+#define CORRECT_PARAMS_DEFAULT \
+{ \
+  .ctpcol = 0, .ctxcol = 0,                      \
+  .frag_len_min = DEFAULT_CRTALN_FRAGLEN_MIN,    \
+  .frag_len_max = DEFAULT_CRTALN_FRAGLEN_MAX,    \
+  .one_way_gap_traverse = true,                  \
+  .use_end_check = true,                         \
+  .max_context = DEFAULT_CRTALN_MAX_CONTEXT,     \
+  .gap_variance = DEFAULT_CRTALN_GAP_VARIANCE,   \
+  .gap_wiggle = DEFAULT_CRTALN_GAP_WIGGLE        \
+}
 
 typedef struct
 {

src/alignment/correct_aln_input.c
@@ -14,7 +14,8 @@ void correct_aln_input_print(const CorrectAlnInput *c)
   if(c->fq_cutoff > 0) sprintf(fqCutoff, "%u", c->fq_cutoff);
   if(c->hp_cutoff > 0) sprintf(hpCutoff, "%u", c->hp_cutoff);
 
-  status("[task] input: %s%s%s", p1, (has_p2 ? ", " : ""), p2);
+  status("[task] input: %s%s%s", futil_inpath_str(p1),
+         (has_p2 ? ", " : ""), futil_inpath_str(p2));
   status("  FASTQ offset: %s, threshold: %s; cut homopolymers: %s",
          fqOffset, fqCutoff, hpCutoff);
 

src/basic/async_read_io.c
@@ -316,7 +316,7 @@ size_t asyncio_input_nkmers(const AsyncIOInput *io)
     if(sf) {
       off_t fsize = futil_get_file_size(sf->path);
       if(fsize < 0) {
-        warn("Cannot get file size: %s", sf->path);
+        // warn("Cannot get file size: %s", sf->path);
         return SIZE_MAX;
       }
       else {

src/basic/cmd.c
@@ -6,31 +6,47 @@
 
 #include <ctype.h>
 
-const char *cmd_usage = NULL;
-char *cmd_line_given = NULL, *cmd_cwd = NULL;
-bool printed_header = false; // If we have called cmd_print_status_header()
+// Global object that clones input arguments
+struct Cmd {
+  // These arrays are allocated
+  char **init_argv;
+  int init_argc;
+  char *cmd_line_str, *cwd;
+  // This is just a pointer -- not allocated
+  const char *usage;
+  bool printed_header; // If we have called cmd_print_status_header()
+} cmd;
 
 void cmd_init(int argc, char **argv)
 {
-  cmd_line_given = cmd_concat_args(argc, argv);
-  cmd_cwd = ctx_malloc(PATH_MAX+1);
-  if(futil_get_current_dir(cmd_cwd) == NULL) {
+  memset(&cmd, 0, sizeof(cmd));
+
+  cmd.init_argc = argc;
+  cmd.init_argv = cmd_clone_args(argc, argv);
+  cmd.cmd_line_str = cmd_concat_args(argc, argv);
+
+  cmd.cwd = ctx_malloc(PATH_MAX+1);
+  if(futil_get_current_dir(cmd.cwd) == NULL) {
     warn("Couldn't get current working dir path");
-    strcpy(cmd_cwd, "./");
+    strcpy(cmd.cwd, "./");
   }
 }
 
 void cmd_destroy()
 {
-  ctx_free(cmd_line_given);
-  ctx_free(cmd_cwd);
-  cmd_usage = cmd_line_given = cmd_cwd = NULL;
+  ctx_free(cmd.init_argv[0]);
+  ctx_free(cmd.init_argv);
+  ctx_free(cmd.cmd_line_str);
+  ctx_free(cmd.cwd);
+  memset(&cmd, 0, sizeof(cmd));
 }
 
-void cmd_set_usage(const char *usage) { cmd_usage = usage; }
-const char* cmd_get_usage() { return cmd_usage; }
-const char* cmd_get_cmdline() { return cmd_line_given; }
-const char* cmd_get_cwd() { return cmd_cwd; }
+void cmd_set_usage(const char *usage) { cmd.usage = usage; }
+const char*  cmd_get_usage() { return cmd.usage; }
+const char*  cmd_get_cmdline() { return cmd.cmd_line_str; }
+const char*  cmd_get_cwd() { return cmd.cwd; }
+const char** cmd_get_argv() { return (const char**)cmd.init_argv; }
+int          cmd_get_argc() { return cmd.init_argc; }
 
 // Print status updates:
 // [cmd] ...
@@ -39,7 +55,7 @@ const char* cmd_get_cwd() { return cmd_cwd; }
 void cmd_print_status_header()
 {
   // Print 1) command used 2) current working directory 3) version info
-  printed_header |= (ctx_msg_out != NULL);
+  cmd.printed_header |= (ctx_msg_out != NULL);
   status("[cmd] %s", cmd_get_cmdline());
   status("[cwd] %s", cmd_get_cwd());
   status("[version] "VERSION_STATUS_STR" k=%i..%i",
@@ -47,20 +63,20 @@ void cmd_print_status_header()
 }
 
 void cmd_get_longopt_str(const struct option *longs, char shortopt,
-                         char *cmd, size_t buflen)
+                         char *cmdstr, size_t buflen)
 {
   const char def_str[] = "-X, --Unknown";
   ctx_assert(buflen >= strlen(def_str)+1);
 
-  string_safe_ncpy(cmd, def_str, buflen);
-  cmd[1] = shortopt;
+  string_safe_ncpy(cmdstr, def_str, buflen);
+  cmdstr[1] = shortopt;
 
   size_t i;
   for(i = 0; longs[i].name != NULL; i++) {
     if(longs[i].val == shortopt) {
-      cmd[0] = '-';
-      cmd[1] = shortopt;
-      string_safe_ncpy(cmd+6, longs[i].name, buflen-6);
+      cmdstr[0] = '-';
+      cmdstr[1] = shortopt;
+      string_safe_ncpy(cmdstr+6, longs[i].name, buflen-6);
       break;
     }
   }
@@ -83,102 +99,103 @@ void cmd_long_opts_to_short(const struct option *longs,
   opts[j] = '\0';
 }
 
-double cmd_udouble(const char *cmd, const char *arg)
+double cmd_udouble(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
   double tmp;
   if(!parse_entire_double(arg, &tmp))
-    cmd_print_usage("%s requires a double >= 0: %s", cmd, arg);
+    cmd_print_usage("%s requires a double >= 0: %s", cmdstr, arg);
   return tmp;
 }
 
-double cmd_udouble_nonzero(const char *cmd, const char *arg)
+double cmd_udouble_nonzero(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
   double tmp;
   if(!parse_entire_double(arg, &tmp) || tmp <= 0)
-    cmd_print_usage("%s requires a double > 0: %s", cmd, arg);
+    cmd_print_usage("%s requires a double > 0: %s", cmdstr, arg);
   return tmp;
 }
 
-uint8_t cmd_uint8(const char *cmd, const char *arg)
+uint8_t cmd_uint8(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
   unsigned int tmp;
   if(!parse_entire_uint(arg, &tmp))
-    cmd_print_usage("%s requires an int 0 <= x < 255: %s", cmd, arg);
+    cmd_print_usage("%s requires an int 0 <= x < 255: %s", cmdstr, arg);
   return tmp;
 }
 
-int32_t cmd_int32(const char *cmd, const char *arg)
+int32_t cmd_int32(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
   int tmp;
   if(!parse_entire_int(arg, &tmp))
-    cmd_print_usage("%s requires an int: %s", cmd, arg);
+    cmd_print_usage("%s requires an int: %s", cmdstr, arg);
   return tmp;
 }
 
-uint32_t cmd_uint32(const char *cmd, const char *arg)
+uint32_t cmd_uint32(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
   unsigned int tmp;
   if(!parse_entire_uint(arg, &tmp))
-    cmd_print_usage("%s requires an int x >= 0: %s", cmd, arg);
+    cmd_print_usage("%s requires an int x >= 0: %s", cmdstr, arg);
   return tmp;
 }
 
-uint32_t cmd_uint32_nonzero(const char *cmd, const char *arg)
+uint32_t cmd_uint32_nonzero(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
-  uint32_t n = cmd_uint32(cmd, arg);
-  if(n == 0) cmd_print_usage("%s <N> must be > 0: %s", cmd, arg);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
+  uint32_t n = cmd_uint32(cmdstr, arg);
+  if(n == 0) cmd_print_usage("%s <N> must be > 0: %s", cmdstr, arg);
   return n;
 }
 
-size_t cmd_size(const char *cmd, const char *arg)
+size_t cmd_size(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
   size_t tmp;
   if(!parse_entire_size(arg, &tmp))
-    cmd_print_usage("%s requires an int x >= 0: %s", cmd, arg);
+    cmd_print_usage("%s requires an int x >= 0: %s", cmdstr, arg);
   return tmp;
 }
 
-size_t cmd_size_nonzero(const char *cmd, const char *arg)
+size_t cmd_size_nonzero(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
-  size_t n = cmd_size(cmd, arg);
-  if(n == 0) cmd_print_usage("%s <N> must be > 0: %s", cmd, arg);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
+  size_t n = cmd_size(cmdstr, arg);
+  if(n == 0) cmd_print_usage("%s <N> must be > 0: %s", cmdstr, arg);
   return n;
 }
 
-size_t cmd_parse_arg_mem(const char *cmd, const char *arg)
+size_t cmd_parse_arg_mem(const char *cmdstr, const char *arg)
 {
-  ctx_assert(cmd != NULL);
-  ctx_assert2(arg != NULL, "cmd: %s", cmd);
+  ctx_assert(cmdstr != NULL);
+  ctx_assert2(arg != NULL, "cmd: %s", cmdstr);
   size_t mem;
   if(!mem_to_integer(arg, &mem))
-    cmd_print_usage("%s %s valid options: 1024 2MB 1G", cmd, arg);
+    cmd_print_usage("%s %s valid options: 1024 2MB 1G", cmdstr, arg);
   return mem;
 }
 
-seq_format cmd_parse_format(const char *cmd, const char *arg)
+seq_format cmd_parse_format(const char *cmdstr, const char *arg)
 {
   int tmp = seqout_str2fmt(optarg);
   if(tmp != SEQ_FMT_FASTQ && tmp != SEQ_FMT_FASTA && tmp != SEQ_FMT_PLAIN)
-    cmd_print_usage("Invalid %s {FASTA,FASTQ,PLAIN} option: %s", cmd, arg);
+    cmd_print_usage("Invalid %s {FASTA,FASTQ,PLAIN} option: %s", cmdstr, arg);
   return (seq_format)tmp;
 }
 
+// Beware: we don't escape spaces etc.
 // Remember to free return value
 char* cmd_concat_args(int argc, char **argv)
 {
@@ -201,11 +218,37 @@ char* cmd_concat_args(int argc, char **argv)
   return cmdline;
 }
 
+// Remember to free pointers and strings
+//    char **args = cmd_clone_args(argc, argv);
+//    free(args[0]);
+//    free(args);
+char** cmd_clone_args(int argc, char **argv)
+{
+  int i; size_t len = 0; char *str;
+
+  for(i = 0; i < argc; i++)
+    len += strlen(argv[i]) + 1;
+
+  char *cmds_merged = ctx_malloc(len);
+  char **cmd_ptrs = ctx_malloc(argc * sizeof(char*));
+
+  for(str = cmds_merged, i = 0; i < argc; i++) {
+    cmd_ptrs[i] = str;
+    len = strlen(argv[i]);
+    memcpy(str, argv[i], len);
+    str[len] = '\0';
+    str += len+1; // length + null byte
+  }
+
+  return cmd_ptrs;
+}
+
+
 void cmd_print_usage(const char *errfmt,  ...)
 {
   ctx_msg_out = stderr;
 
-  if(!printed_header)
+  if(!cmd.printed_header)
     cmd_print_status_header();
 
   pthread_mutex_lock(&ctx_biglock); // lock is never released
@@ -221,6 +264,6 @@ void cmd_print_usage(const char *errfmt,  ...)
     fputc('\n', stderr);
   }
 
-  fputs(cmd_usage, stderr);
+  fputs(cmd.usage, stderr);
   exit(EXIT_FAILURE);
 }

src/basic/cmd.h
@@ -20,9 +20,11 @@ void cmd_init(int argc, char **argv);
 void cmd_destroy();
 
 void cmd_set_usage(const char *usage);
-const char* cmd_get_usage();
-const char* cmd_get_cmdline();
-const char* cmd_get_cwd();
+const char*  cmd_get_usage();
+const char*  cmd_get_cmdline();
+const char*  cmd_get_cwd();
+const char** cmd_get_argv();
+int          cmd_get_argc();
 
 // Print status updates:
 // [cmd] ...
@@ -56,8 +58,15 @@ size_t cmd_parse_arg_mem(const char *cmd, const char *arg);
 
 seq_format cmd_parse_format(const char *cmd, const char *arg);
 
+// Beware: we don't escape spaces etc.
 // Remember to free return value
 char* cmd_concat_args(int argc, char **argv);
+
+// Remember to free pointers and strings
+//    char **args = cmd_clone_args(argc, argv);
+//    free(args[0]);
+//    free(args);
+char** cmd_clone_args(int argc, char **argv);
 
 void cmd_print_usage(const char *errfmt,  ...)
   __attribute__((noreturn))

commit e97aaa96c4f5ac8470c8e0084a1685d8be761bae

Fix bug in mate pair gap traversal, introduced by 93ae0d7c85b31edbbf10262d3b8990757691fa29

src/alignment/correct_alignment.c
@@ -397,8 +397,12 @@ dBNodeBuffer* correct_alignment_nxt(CorrectAlnWorker *wrkr)
     is_mp = (both_reads && wrkr->gap_idx == aln->r2strtidx);
 
     // gap_est is how many kmers we lost through low qual scores, hp runs etc.
-    gap_est = aln_rpos[wrkr->gap_idx] - aln_rpos[wrkr->gap_idx-1];
-    if(is_mp) gap_est += aln->r1enderr;
+    if(is_mp) {
+      ctx_assert(aln->r1enderr == db_aln_r1enderr(aln,kmer_size));
+      gap_est = aln->r1enderr + aln_rpos[wrkr->gap_idx];
+    } else {
+      gap_est = aln_rpos[wrkr->gap_idx] - aln_rpos[wrkr->gap_idx-1];
+    }
 
     // Wiggle due to variation / error
     long wiggle = gap_est * params.gap_variance + params.gap_wiggle;

src/alignment/db_alignment.c
@@ -42,7 +42,7 @@ static size_t db_alignment_from_read(dBAlignment *alignment, const read_t *r,
   Int32Buffer *rpos = &alignment->rpos;
 
   ctx_assert(nodes->len == rpos->len);
-  size_t n = nodes->len;
+  size_t n = nodes->len, init_len = n;
 
   db_node_buf_capacity(nodes, n + r->seq.end);
   int32_buf_capacity(rpos, n + r->seq.end);
@@ -79,8 +79,8 @@ static size_t db_alignment_from_read(dBAlignment *alignment, const read_t *r,
   }
 
   // Return number of bases from the last kmer found until read end
-  size_t ret = (n == rpos->len ? r->seq.end /* No kmers found */
-                               : r->seq.end - (rpos->data[n] + kmer_size-1));
+  size_t ret = (n == init_len ? r->seq.end /* No kmers found */
+                              : r->seq.end - (rpos->data[n-1] + kmer_size));
 
   nodes->len = rpos->len = n;
 

src/alignment/db_alignment.h
@@ -24,8 +24,8 @@ typedef struct
   // whether or not there are sequencing gaps
   bool seq_gaps;
   // if used_r1 && used_r2
-  // gap between r1 and r2: nodes[r2strtindx-1] .. nodes[r2strtindx]
-  // = r1enderr + insgapsize + rpos[r2strtindx]
+  // gap between r1 and r2: nodes[r2strtidx-1] .. nodes[r2strtidx]
+  // = r1enderr + insgapsize + rpos[r2strtidx]
   int colour; // -1 if colour agnostic, otherwise only nodes in colour used
 } dBAlignment;
 
@@ -34,6 +34,9 @@ size_t db_alignment_est_mem();
 
 void db_alignment_alloc(dBAlignment *alignment);
 void db_alignment_dealloc(dBAlignment *alignment);
+
+#define db_aln_r1enderr(aln,k) ((aln)->r1bases - \
+  ((aln)->r2strtidx > 0 ? (aln)->rpos.data[(aln)->r2strtidx-1] + (k) : 0))
 
 // if colour is -1 aligns to all colours, otherwise aligns to given colour only
 // Assumes both reads are in FF orientation

src/global/ctx_alloc.c
@@ -14,8 +14,9 @@ static inline void _oom(void *ptr, size_t nel, size_t elsize,
   char memstr[50];
   if(SIZE_MAX / elsize < nel) strcpy(memstr, "overflow");
   else bytes_to_str(nel*elsize, 1, memstr);
-  dief(file, func, line, "Out of memory (%p, %zu x %zu = %s)",
-       ptr, nel, elsize, memstr);
+  warnf(file, func, line, "Out of memory (%p, %zu x %zu = %s)",
+        ptr, nel, elsize, memstr);
+  abort();
 }
 
 // If `zero` is true and ptr is NULL call calloc, otherwise realloc

src/graph/graph_walker.h
@@ -6,6 +6,12 @@
 #include "gpath_store.h"
 #include "gpath_follow.h"
 
+typedef struct
+{
+  uint8_t inleft:1, outleft:1, inright:1, outright:1;
+  size_t num_nodes;
+} GraphSection;
+
 // Result from graph_walker_choose
 typedef struct
 {