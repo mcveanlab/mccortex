cbf913827a84188349ab338c2d5bbebe5e3aabda
Isaac Turner <turner.isaac@gmail.com>
2015-02-17 11:05:40 +0000

    Tell Travis CI to install JSON perl module

--- travis/install.sh
+++ travis/install.sh
@@ -6,5 +6,8 @@ RUN_TRAVIS=`./travis/run.sh`
 
 if [ "$RUN_TRAVIS" == "yes" ]
 then
+  # Fetch third party code required to compile
   cd libs && make core && cd ..
+  # Install perl JSON module
+  cpanm JSON
 fi
