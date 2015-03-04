mcveanlab/mccortex

commit e0e49c08a8052c379b7e0a00ff5d5364da90dad4

VCFs pass vcf-validator; Fix bug in tests/pipeline seq files; calls2vcf: Add kmer-size to VCF lines

src/commands/ctx_calls2vcf.c
@@ -78,7 +78,9 @@ const char *sam_path = NULL;
 // Things we figure out by looking at the input
 //
 bool input_bubble_format = false;
-size_t kmer_size, num_samples;
+size_t kmer_size;
+// samples in VCF, (0 for bubble, does not include ref in breakpoint calls)
+size_t num_samples;
 
 //
 // Reference genome
@@ -785,6 +787,7 @@ static void align_entry(const CallFileEntry *centry, const char *callid,
   char info[200];
   strcpy(info, input_bubble_format ? "BUBBLE=" : "BRKPNT=");
   strcpy(info+7, callid);
+  sprintf(info+strlen(info), ";K%zu", kmer_size);
 
   size_t nlines = call_file_num_lines(centry);
 
@@ -931,7 +934,8 @@ static cJSON* read_input_header(gzFile gzin)
 }
 
 /**
- * @return number of samples found in json header
+ * @return number of samples found in json header,
+ *         not including ref if breakpoint calls
  */
 static size_t print_vcf_header(cJSON *json, bool is_breakpoint, FILE *fout)
 {
@@ -958,7 +962,7 @@ static size_t print_vcf_header(cJSON *json, bool is_breakpoint, FILE *fout)
   }
 
   // Print command entry for this command
-  fprintf(fout, "##CMD=<key=\"%s\",prev=\"%s\",cmd=\"%s\",cwd=\"%s\">\n",
+  fprintf(fout, "##mccortex=<key=\"%s\",prev=\"%s\",cmd=\"%s\",cwd=\"%s\">\n",
           hex_rand_str(keystr, sizeof(keystr)),
           prevstr ? prevstr : "NULL",
           cmd_get_cmdline(), cmd_get_cwd());
@@ -972,7 +976,7 @@ static size_t print_vcf_header(cJSON *json, bool is_breakpoint, FILE *fout)
     cJSON *prev = json_hdr_get(command, "prev", cJSON_Array,  input_path);
     prev = prev->child; // result could be NULL
     if(prev && prev->type != cJSON_String) die("Invalid 'prev' field");
-    fprintf(fout, "##CMD=<key=\"%s\",prev=\"%s", key->valuestring,
+    fprintf(fout, "##mccortex=<key=\"%s\",prev=\"%s", key->valuestring,
                   prev ? prev->valuestring : "NULL");
     if(prev) {
       while((prev = prev->next) != NULL) fprintf(fout, ";%s", prev->valuestring);
@@ -991,12 +995,19 @@ static size_t print_vcf_header(cJSON *json, bool is_breakpoint, FILE *fout)
   else
     fprintf(fout, "##INFO=<ID=BUBBLE,Number=1,Type=String,Description=\"Bubble call\">\n");
 
+  fprintf(fout, "##INFO=<ID=K%zu,Number=0,Type=Flag,Description=\"Found at k=%zu\">\n", kmer_size, kmer_size);
+
   fprintf(fout, "##FORMAT=<ID=GT,Number=1,Type=String,Description=\"Genotype\">\n");
   fprintf(fout, "##FILTER=<ID=PASS,Description=\"All filters passed\">\n");
 
+  // Print reference paths
+  fprintf(fout, "##reference=%s", ref_paths[0]);
+  for(i = 1; i < num_ref_paths; i++) printf(",%s", ref_paths[i]);
+  fprintf(fout, "\n");
+
   // Print contigs lengths
   for(i = 0; i < chroms.len; i++) {
-    fprintf(fout, "##contig=<id=%s,length=%zu>\n",
+    fprintf(fout, "##contig=<ID=%s,length=%zu>\n",
             chroms.data[i].name.b, chroms.data[i].seq.end);
   }
 
@@ -1013,7 +1024,10 @@ static size_t print_vcf_header(cJSON *json, bool is_breakpoint, FILE *fout)
     cJSON *colour_json  = colours_json->child;
     if(colour_json == NULL) die("Missing colours");
 
-    for(; colour_json; colour_json = colour_json->next, nsamples_printed++) {
+    // Don't print the last colour (it's the ref)
+    for(; colour_json && colour_json->next;
+        colour_json = colour_json->next, nsamples_printed++)
+    {
       cJSON *sample_json = json_hdr_get(colour_json, "sample", cJSON_String, input_path);
       fputc('\t', fout);
       fputs(sample_json->valuestring, fout);

scripts/make-pipeline.pl
@@ -138,6 +138,12 @@ CLEANING_ARGS=
 LINK_CLEAN_NKMERS='.$default_link_clean_nkmers.'
 REF_FILE='.(defined($ref_path) ? $ref_path : '').'
 
+ifdef NKMERS
+  CTX_ARGS=-m $(MEM) -t $(NTHREADS) -n $(NKMERS)
+else
+  CTX_ARGS=-m $(MEM) -t $(NTHREADS)
+endif
+
 # Paths to scripts
 CTXLINKS=$(CTXDIR)/scripts/cortex_links.pl
 MEDIAN_LINK_THRESH=$(CTXDIR)/scripts/median-link-threshold.sh
@@ -269,7 +275,7 @@ for my $k (@kmers) {
   if(defined($ref_path)) {
     print "# reference at k=$k\n";
     print "$proj/k$k/ref/ref.ctx: $ref_path\n";
-    print "\t$ctx build -m \$(MEM) -t \$(NTHREADS) -k $k -s \$< \$@ >& \$@.log\n\n";
+    print "\t$ctx build \$(CTX_ARGS) -k $k -s \$< \$@ >& \$@.log\n\n";
   }
 
   print "# building graphs at k=$k\n";
@@ -279,7 +285,7 @@ for my $k (@kmers) {
     my @files = get_all_sample_files($sample);
 
     print "$proj/k$k/graphs/$sname.raw.ctx: ".join(' ', @files)." | \$(DIRS)\n";
-    print "\t$ctx build -m \$(MEM) -t \$(NTHREADS) -k $k --sample $sname " .
+    print "\t$ctx build \$(CTX_ARGS) -k $k --sample $sname " .
           join(' ', (map {"--seq $_"}               @{$sample->{'se_files'}}),
                     (map {"--seq2 $_->[0]:$_->[1]"} @{$sample->{'pe_files'}}),
                     (map {"--seqi $_"}              @{$sample->{'i_files'}})) .
@@ -289,8 +295,8 @@ for my $k (@kmers) {
   # Clean graph files at k=$k
   print "# graph cleaning at k=$k\n";
   print "$proj/k$k/graphs/%.raw.covg.csv $proj/k$k/graphs/%.clean.ctx: $proj/k$k/graphs/%.raw.ctx\n";
-  print "\t($ctx clean -m \$(MEM) -t \$(NTHREADS) --covg-before $proj/k$k/graphs/\$*.raw.covg.csv -o $proj/k$k/graphs/\$*.clean.ctx \$(CLEANING_ARGS) \$<; \\\n";
-  print "\t $ctx inferedges -m \$(MEM) -t \$(NTHREADS) $proj/k$k/graphs/\$*.clean.ctx) >& $proj/k$k/graphs/\$*.clean.ctx.log\n\n";
+  print "\t($ctx clean \$(CTX_ARGS) --covg-before $proj/k$k/graphs/\$*.raw.covg.csv -o $proj/k$k/graphs/\$*.clean.ctx \$(CLEANING_ARGS) \$<; \\\n";
+  print "\t $ctx inferedges \$(CTX_ARGS) $proj/k$k/graphs/\$*.clean.ctx) >& $proj/k$k/graphs/\$*.clean.ctx.log\n\n";
 }
 
 # Create and clean link files
@@ -313,7 +319,7 @@ for my $k (@kmers) {
 
     # Single ended threading
     print "$ctp_se_raw_file: $ctx_clean_file @se_files | \$(DIRS)\n";
-    print "\t$ctx thread -m \$(MEM) -t \$(NTHREADS) " .
+    print "\t$ctx thread \$(CTX_ARGS) " .
           join(' ', (map {"--seq $_"}                    @{$sample->{'se_files'}}),
                     (map {"--seq $_->[0] --seq $_->[1]"} @{$sample->{'pe_files'}}),
                     (map {"--seq $_"}                    @{$sample->{'i_files'}})) .
@@ -321,7 +327,7 @@ for my $k (@kmers) {
 
     if(@{$sample->{'pe_files'}} > 0 || @{$sample->{'i_files'}} > 0) {
       print "$ctp_pe_raw_file: $ctx_clean_file $ctp_se_clean_file @pe_files | \$(DIRS)\n";
-      print "\t$ctx thread -m \$(MEM) -t \$(NTHREADS) -p $ctp_se_clean_file " .
+      print "\t$ctx thread \$(CTX_ARGS) -p $ctp_se_clean_file " .
               join(' ', (map {"--seq2 $_->[0]:$_->[1]"} @{$sample->{'pe_files'}}),
                         (map {"--seqi $_"}              @{$sample->{'i_files'}})) .
               ' -o $@ $< >& $@.log'."\n\n";
@@ -361,7 +367,7 @@ for my $k (@kmers) {
   my $ctp_txt = get_p_args($k);
   print "# bubble calls k=$k\n";
   print "$proj/k$k/bubbles/bubbles.txt.gz: \$(CLEAN_GRAPHS_K$k) \$(REF_GRAPH_K$k) \$(CLEAN_PE_LINKS_K$k) | \$(DIRS)\n";
-  print "\t$ctx bubbles -m \$(MEM) -t \$(NTHREADS) -o \$@ $ctp_txt \$(CLEAN_GRAPHS_K$k) \$(REF_GRAPH_K$k) >& \$@.log\n\n";
+  print "\t$ctx bubbles \$(CTX_ARGS) -o \$@ $ctp_txt \$(CLEAN_GRAPHS_K$k) \$(REF_GRAPH_K$k) >& \$@.log\n\n";
 }
 
 # Some things require a reference to be used
@@ -376,7 +382,7 @@ if(defined($ref_path))
 
     print "# breakpoint calls k=$k\n";
     print "$brkpnt_file: \$(CLEAN_GRAPHS_K$k) \$(REF_GRAPH_K$k) \$(CLEAN_PE_LINKS_K$k) | \$(DIRS)\n";
-    print "\t$ctx breakpoints -m \$(MEM) -t \$(NTHREADS) -s \$(REF_FILE) -o \$@ $ctp_txt \$(CLEAN_GRAPHS_K$k) \$(REF_GRAPH_K$k) >& \$@.log\n\n";
+    print "\t$ctx breakpoints \$(CTX_ARGS) -s \$(REF_FILE) -o \$@ $ctp_txt \$(CLEAN_GRAPHS_K$k) \$(REF_GRAPH_K$k) >& \$@.log\n\n";
   }
 
   # Generate buble VCFs
@@ -466,7 +472,7 @@ sub get_all_sample_files
 
 sub get_required_binaries
 {
-  return map { get_ctx($_) } keys get_maxk_values(@_);
+  return map { get_ctx($_) } get_maxk_values(@_);
 }
 
 sub get_maxk_values

tests/pipeline/Makefile
@@ -12,21 +12,21 @@ SEQ_FILES=ref.fa ben.fa jerry.fa \
           jerry.1.fa jerry.2.fa
 
 all: job.mk $(SEQ_FILES)
-	$(MAKE) -f job.mk CTXDIR=$(CTXDIR) MEM=100M CLEANING_ARGS='--supernodes=1' all
+	$(MAKE) -f job.mk CTXDIR=$(CTXDIR) MEM=200M NKMERS=1M CLEANING_ARGS='--supernodes=1' all
 
 # 600 random bases generated with: dnacat -n 600
-#............................................................................V........................................................................................*...........................................................................................................VV.................................................................................................................................................*....................................................................................................................VVVV..........................................................
+#.............................................................................V................................................V.......................................*...........................................................................................................VV.................................................................................................................................................*....................................................................................................................VVVV..........................................................
 ref.fa:
-	( echo '>ref'; echo '\
-GCGTTACAATATCGTATTGGGTTCGTGACCAACACTCCCATTTCTTGATATGACGCCATCAAACGACTACACGGAGACCCGGCCGAGCATGGCAACCCGCACGACTGCATCATCTCCATCAATCCAACCATACTCCCGGACTTACCCCTGCCCCGGGCGCAGCAGTCCTTAAGATCAGGAACTGGGGTGTACGACGGCCTCGCTGACACGGTACCAGCCGTGCACCGATGCTGCTAGGCACCCGTCGCCTGCTCAAGAAATGGCTGGGTTCAATAGAAGCGTTTGTGAGTGCTTCGACTCGTTAGGATGTAATTAGGGCCAGTAGTCAACCAGCGCTAGTGAGAATATGATAGAGATTTCGCAAAGTCCTTGGTATACAGGATCTCAACCCACAGACTGCGGAGGCTGTGGTGCCATCATCGGACTCACTACGTCCTTGTCAGGCCTAACCTTTCAGGGCGGCAAGCTACGGTTACCTGACCGAAGTCTTATTCACAGTTCGGTAGCTCCAATCATTGCGAGGTTAGCTTAACGCCTGACATTACCTGGCAAACATGCTCCTTTCACGACCGTTTATCGGCGCGATTTGATATCCACTTG' | tr -d '-'; ) > $@
+	( echo '>ref'; echo \
+'GCGTTACAATATCGTATTGGGTTCGTGACCAACACTCCCATTTCTTGATATGACGCCATCAAACGACTACACGGAGACCCGGCCGAGCATGGCAACCCGCACGACTGCATCATCTCCATCAATCCA-CCATACTCCCGGACTTACCCCTGCCCCGGGCGCAGCAGTCCTTAAGATCAGGAACTGGGGTGTACGACGGCCTCGCTGACACGGTACCAGCCGTGCACCGATGCTGCTAGGCACCCGTCGCCTGCTCAAGAAATGGCTGGGTTCAATAGAAGCGTTTGTGAGTGCTTCGACTCGTTAGGATGTAATTAGGGCCAGTAGTCAACCAGCGCTAGTGAGAATATGATAGAGATTTCGCAAAGTCCTTGGTATACAGGATCTCAACCCACAGACTGCGGAGGCTGTGGTGCCATCATCGGACTCACTACGTCCTTGTCAGGCCTAACCTTTCAGGGCGGCAAGCTACGGTTACCTGACCGAAGTCTTATTCACAGTTCGGTAGCTCCAATCATTGCGAGGTTAGCTTAACGCCTGACATTACCTGGCAAACATGCTCCTTTCACGACCGTTTATCGGCGCGATTTGATATCCACTTG' | tr -d '-'; ) > $@
 
 ben.fa:
-	( echo '>ben'; echo '\
-GCGTTACAATATCGTATTGGGTTCGTGACCAACACTCCCATTTCTTGATATGACGCCATCAAACGACTACACGGAGA-CCGGCCGAGCATGGCAACCCGCACGACTGCATCATCTCCATCAATCCAACCATACTCCCGGACTTACCCCTGCCCCGGGCGCAGCAGTCCTTAAGATCAGGAACTGGGGTGTACGACGGCCTCGCTGACACGGTACCAGCCGTGCACCGATGCTGCTAGGCACCCGTCGCCTGCTCAAGAAATGGCTGGGTTCAAT--AAGCGTTTGTGAGTGCTTCGACTCGTTAGGATGTAATTAGGGCCAGTAGTCAACCAGCGCTAGTGAGAATATGATAGAGATTTCGCAAAGTCCTTGGTATACAGGATCTCAACCCACAGACTGCGGAGGCTGTGGTGCCATCATCGGACTCACTACGTCCTTGTCAGGCCTAACCTTTCAGGGCGGCAAGCTACGGTTACCTGACCGAAGTCTTATTCACAGTTCGGTAGCTCCAATCATTGCGAGGTTAGCTTAACGCCTGACATTACCTGGCAAACATGCTCCTTTCACGACCGTTTATCGGCGCGATTTGATATCCACTTG' | tr -d '-'; ) > $@
+	( echo '>ben'; echo \
+'GCGTTACAATATCGTATTGGGTTCGTGACCAACACTCCCATTTCTTGATATGACGCCATCAAACGACTACACGGAGA-CCGGCCGAGCATGGCAACCCGCACGACTGCATCATCTCCATCAATCCAACCATACTCCCGGACTTACCCCTGCCCCGGGCGCAGCAGTCCTTAAGATCAGGAACTGGGGTGTACGACGGCCTCGCTGACACGGTACCAGCCGTGCACCGATGCTGCTAGGCACCCGTCGCCTGCTCAAGAAATGGCTGGGTTCAAT--AAGCGTTTGTGAGTGCTTCGACTCGTTAGGATGTAATTAGGGCCAGTAGTCAACCAGCGCTAGTGAGAATATGATAGAGATTTCGCAAAGTCCTTGGTATACAGGATCTCAACCCACAGACTGCGGAGGCTGTGGTGCCATCATCGGACTCACTACGTCCTTGTCAGGCCTAACCTTTCAGGGCGGCAAGCTACGGTTACCTGACCGAAGTCTTATTCACAGTTCGGTAGCTCCAATCATTGCGAGGTTAGCTTAACGCCTGACATTACCTGGCAAACATGCTCCTTTCACGACCGTTTATCGGCGCGATTTGATATCCACTTG' | tr -d '-'; ) > $@
 
 jerry.fa:
-	( echo '>jerry'; echo '\
-GCGTTACAATATCGTATTGGGTTCGTGACCAACACTCCCATTTCTTGATATGACGCCATCAAACGACTACACGGAGACCCGGCCGAGCATGGCAACCCGCACGACTGCATCATCTCCATCAATCCAACCATACTCCCGGACTTACCCCTGCCCCGGGCGCAGCAGTaCTTAAGATCAGGAACTGGGGTGTACGACGGCCTCGCTGACACGGTACCAGCCGTGCACCGATGCTGCTAGGCACCCGTCGCCTGCTCAAGAAATGGCTGGGTTCAATAGAAGCGTTTGTGAGTGCTTCGACTCGTTAGGATGTAATTAGGGCCAGTAGTCAACCAGCGCTAGTGAGAATATGATAGAGATTTCGCAAAGTCCTTGGTATACAGGATCTCAACCCACAGACTGCGGAGGCTGTGGTGCCATCATCtGACTCACTACGTCCTTGTCAGGCCTAACCTTTCAGGGCGGCAAGCTACGGTTACCTGACCGAAGTCTTATTCACAGTTCGGTAGCTCCAATCATTGCGAGGTTAGCTTAACGCCTG----TACCTGGCAAACATGCTCCTTTCACGACCGTTTATCGGCGCGATTTGATATCCACTTG' | tr -d '-'; ) > $@
+	( echo '>jerry'; echo \
+'GCGTTACAATATCGTATTGGGTTCGTGACCAACACTCCCATTTCTTGATATGACGCCATCAAACGACTACACGGAGACCCGGCCGAGCATGGCAACCCGCACGACTGCATCATCTCCATCAATCCAACCATACTCCCGGACTTACCCCTGCCCCGGGCGCAGCAGTaCTTAAGATCAGGAACTGGGGTGTACGACGGCCTCGCTGACACGGTACCAGCCGTGCACCGATGCTGCTAGGCACCCGTCGCCTGCTCAAGAAATGGCTGGGTTCAATAGAAGCGTTTGTGAGTGCTTCGACTCGTTAGGATGTAATTAGGGCCAGTAGTCAACCAGCGCTAGTGAGAATATGATAGAGATTTCGCAAAGTCCTTGGTATACAGGATCTCAACCCACAGACTGCGGAGGCTGTGGTGCCATCATCtGACTCACTACGTCCTTGTCAGGCCTAACCTTTCAGGGCGGCAAGCTACGGTTACCTGACCGAAGTCTTATTCACAGTTCGGTAGCTCCAATCATTGCGAGGTTAGCTTAACGCCTG----TACCTGGCAAACATGCTCCTTTCACGACCGTTTATCGGCGCGATTTGATATCCACTTG' | tr -d '-'; ) > $@
 
 %.se.fa: %.fa
 	$(READSIM) -d 20 -l 100 -s -r $< tmp

commit 35761bb8860b8a974a87d661539cf0bb18544f3e

ctx_calls2vcf.c: Undo bug introduced in e0e49c08a805

src/commands/ctx_calls2vcf.c
@@ -1024,9 +1024,7 @@ static size_t print_vcf_header(cJSON *json, bool is_breakpoint, FILE *fout)
     cJSON *colour_json  = colours_json->child;
     if(colour_json == NULL) die("Missing colours");
 
-    // Don't print the last colour (it's the ref)
-    for(; colour_json && colour_json->next;
-        colour_json = colour_json->next, nsamples_printed++)
+    for(; colour_json; colour_json = colour_json->next, nsamples_printed++)
     {
       cJSON *sample_json = json_hdr_get(colour_json, "sample", cJSON_String, input_path);
       fputc('\t', fout);
@@ -1114,6 +1112,8 @@ int ctx_calls2vcf(int argc, char **argv)
 
   // Run
   num_samples = print_vcf_header(json, !input_bubble_format, fout);
+  status("Reading %s call file with %zu samples",
+         input_bubble_format ? "Bubble" : "Breakpoint", num_samples);
   parse_entries(gzin, fout);
 
   // Print stats