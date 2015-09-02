2769e3e157c86b63a7c16e0668129f42250c513c
Isaac Turner <turner.isaac@gmail.com>
2015-09-02 16:20:19 +0100

    Try to get Travis working with mac & linux
    Attempt to install JSON perl module on both mac and linux
    
    == Missed commit message from previous commit fea28bdcbd8e314cf8d4b0f8306fe8388f2392a1
    
    Fixed issues with miscalculating required memory for loading link files
    
    Fixed bug under-estimating required memory for loading link files
    (We were not counting colset bytes)
    
    Added scripts for counting links in files:
    + scripts/links-bytes.sh
    + scripts/links-count.sh

--- travis/install.sh
+++ travis/install.sh
@@ -6,6 +6,23 @@ RUN_TRAVIS=`./travis/run.sh`
 
 if [ "$RUN_TRAVIS" == "yes" ]
 then
+
+  # Install JSON perl package
+  # cpanm JSON    <- Only available when language is `perl`
+  if [ "$TRAVIS_OS_NAME" == "osx" ]
+  then
+    # brew update
+    # brew outdated <package-name> || brew upgrade <package-name>
+    # cpanm JSON
+    sudo cpan JSON
+  elif [ "$TRAVIS_OS_NAME" == "linux" ]
+  then
+    sudo apt-get install libjson-pp-perl
+  else
+    echo "Unexpected TRAVIS_OX_NAME: $TRAVIS_OS_NAME"
+    exit -1
+  fi
+
   # Fetch third party code required to compile
-  cd libs && make core && cd ..
+  cd libs && make && cd ..
 fi
