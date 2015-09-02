b011b0d83b2c67531f321f5c0104a77f74de9dd8
Isaac Turner <turner.isaac@gmail.com>
2015-09-02 21:35:38 +0100

    Travis CI: Attempting local installation of perl dependencies

--- travis/install.sh
+++ travis/install.sh
@@ -7,27 +7,14 @@ RUN_TRAVIS=`./travis/run.sh`
 if [ "$RUN_TRAVIS" == "yes" ]
 then
 
-  # Install JSON perl package
-  # cpanm JSON    <- Only available when language is `perl`
+  # Set up installing perl modules locally using local::lib
+  eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"
 
-  # Set up cpanm
-  curl -L https://cpanmin.us | sudo perl - --sudo App::cpanminus
-  sudo cpanm JSON
-
-  # if [ "$TRAVIS_OS_NAME" == "osx" ]
-  # then
-  #   # brew update
-  #   # brew outdated <package-name> || brew upgrade <package-name>
-  #   # cpanm JSON
-  #   # sudo cpan JSON
-  # elif [ "$TRAVIS_OS_NAME" == "linux" ]
-  # then
-  #   sudo apt-get install libjson-pp-perl
-  # else
-  #   echo "Unexpected TRAVIS_OX_NAME: $TRAVIS_OS_NAME"
-  #   exit -1
-  # fi
+  # Set up cpanm, install JSON perl package
+  curl -L https://cpanmin.us | perl - App::cpanminus
+  cpanm JSON
 
   # Fetch third party code required to compile
   cd libs && make && cd ..
+
 fi
