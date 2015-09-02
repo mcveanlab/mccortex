a32dc29a16882a047064b2d2534726c857b95e48
Isaac Turner <turner.isaac@gmail.com>
2015-09-02 21:42:10 +0100

    Travis CI: Fix path to cpanm

--- travis/install.sh
+++ travis/install.sh
@@ -8,11 +8,13 @@ if [ "$RUN_TRAVIS" == "yes" ]
 then
 
   # Set up installing perl modules locally using local::lib
-  eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"
+  # eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"
 
   # Set up cpanm, install JSON perl package
+  # Using default ~/perl5 local directory
   curl -L https://cpanmin.us | perl - App::cpanminus
-  cpanm JSON
+  # ~/perl5/bin/cpanm local::lib
+  ~/perl5/bin/cpanm JSON
 
   # Fetch third party code required to compile
   cd libs && make && cd ..
