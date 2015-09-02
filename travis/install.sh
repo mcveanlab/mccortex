c39ff78fbb37eb7a0f190843be8bd4c0648e3eb9
Isaac Turner <turner.isaac@gmail.com>
2015-09-02 17:40:08 +0100

    Travis CI: Install cpanm, use it to install JSON

--- travis/install.sh
+++ travis/install.sh
@@ -9,20 +9,24 @@ then
 
   # Install JSON perl package
   # cpanm JSON    <- Only available when language is `perl`
-  if [ "$TRAVIS_OS_NAME" == "osx" ]
-  then
-    # brew update
-    # brew outdated <package-name> || brew upgrade <package-name>
-    # cpanm JSON
-    # sudo cpan JSON
-    sudo cpanm JSON
-  elif [ "$TRAVIS_OS_NAME" == "linux" ]
-  then
-    sudo apt-get install libjson-pp-perl
-  else
-    echo "Unexpected TRAVIS_OX_NAME: $TRAVIS_OS_NAME"
-    exit -1
-  fi
+
+  # Set up cpanm
+  curl -L https://cpanmin.us | sudo perl - --sudo App::cpanminus
+  sudo cpanm JSON
+
+  # if [ "$TRAVIS_OS_NAME" == "osx" ]
+  # then
+  #   # brew update
+  #   # brew outdated <package-name> || brew upgrade <package-name>
+  #   # cpanm JSON
+  #   # sudo cpan JSON
+  # elif [ "$TRAVIS_OS_NAME" == "linux" ]
+  # then
+  #   sudo apt-get install libjson-pp-perl
+  # else
+  #   echo "Unexpected TRAVIS_OX_NAME: $TRAVIS_OS_NAME"
+  #   exit -1
+  # fi
 
   # Fetch third party code required to compile
   cd libs && make && cd ..
