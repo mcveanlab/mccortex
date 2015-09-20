7a3a1814bed8e89d5929012a6835ebbdacb00bf0
Isaac Turner <turner.isaac@gmail.com>
2015-09-20 23:08:23 +0100

    Added missing files

---
+++ src/graph/graphs_load.h
@@ -0,0 +1,49 @@
+#ifndef GRAPHS_LOAD_H_
+#define GRAPHS_LOAD_H_
+
+#include "loading_stats.h"
+#include "db_graph.h"
+#include "graph_file_reader.h"
+
+//
+// Load graphs into the de Bruijn graph in memory from disk
+//
+
+// Stucture for specifying how to load data
+typedef struct
+{
+  dBGraph *db_graph;
+  bool boolean_covgs; // Update covg by at most 1
+  bool must_exist_in_graph;
+  const Edges *must_exist_in_edges;
+  // if empty_colours is true an error is thrown if a kmer from a graph file
+  // is already in the graph
+  bool empty_colours;
+} GraphLoadingPrefs;
+
+#define LOAD_GPREFS_INIT(graph) {  \
+  .db_graph = (graph),             \
+  .boolean_covgs = false,          \
+  .must_exist_in_graph = false,    \
+  .must_exist_in_edges = NULL,     \
+  .empty_colours = false}
+
+// Print loading message
+void graph_loading_print_status(const GraphFileReader *file);
+
+// if only_load_if_in_colour is >= 0, only kmers with coverage in existing
+// colour only_load_if_in_colour will be loaded.
+// if clean_colours != 0 an error is thrown if a node already exists
+// returns the number of colours in the binary
+// If stats != NULL, updates:
+//   stats->num_kmers_loaded
+//   stats->total_bases_read
+// If header is != NULL, header will be stored there.  Be sure to free.
+size_t graph_load(GraphFileReader *file, const GraphLoadingPrefs prefs,
+                  LoadingStats *stats);
+
+// Load all files into colour 0
+void graphs_load_files_flat(GraphFileReader *gfiles, size_t num_files,
+                           GraphLoadingPrefs prefs, LoadingStats *stats);
+
+#endif /* GRAPHS_LOAD_H_ */
