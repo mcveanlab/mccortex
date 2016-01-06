e0a15e7da836a2f056afffc4d6dc5722783867ab
Isaac Turner <turner.isaac@gmail.com>
2016-01-06 16:27:24 +0000

    Update NA12878 + kleb analyses [thesis][skip CI]

--- results/klebsiella/kleb_pneumoniae/indels/kleb-plot-hist.R
+++ results/klebsiella/kleb_pneumoniae/indels/kleb-plot-hist.R
@@ -15,10 +15,11 @@ hists[[5]] <- load_data('hists/breakpoints.hist.csv')
 
 names <- c('Platypus','Freebayes','Cortex', 'McCortex Bubbles', 'McCortex Breakpoints')
 colours <- c('red', 'green', 'blue', 'black', 'purple')
-points <- c('o','o','o', 'o', 'o')
+# points <- c('o','o','o', 'o', 'o')
+points <- rep(20, 5)
 
 ncalls <- length(hists)
-lim <- 50
+lim <- 100
 m <- matrix(0, nrow=2*lim+1, ncol=ncalls+1)
 m[,1] <- -lim:lim
 colnames(m) <- c("dist",names)
@@ -38,12 +39,16 @@ for(i in 1:length(hists)) {
 
 # plot
 
+jf=2
 pdf(file="kleb_indels.pdf",width=6,height=6)
 
-plot(x,m[,2],log="y",col=colours[1],pch=points[1], main="Kleb. Pneumonaie Indels",
+plot(jitter(x,factor=jf), m[,2], log="y",
+     col=colours[1],pch=points[1],
+     main="Klebsiella Pneumoniae Indels",
      xlab="indel size", ylab="count (log)")
+
 for(i in 2:length(names)) {
-  points(x,m[,i+1],col=colours[i],pch=points[i]) # ,type='b'
+  points(jitter(x,factor=jf),m[,i+1],col=colours[i],pch=points[i]) # ,type='b'
 }
 legend("topright", names, col=colours, pch=points)
 
