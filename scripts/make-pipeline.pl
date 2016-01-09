5bf4d7a622e9cd98803005e40bdceb7e57bd1769
Isaac Turner <turner.isaac@gmail.com>
2016-01-09 14:47:46 +0000

    pipeline: vcfcov should write single sample VCFs that can be easily merged

--- scripts/make-pipeline.pl
+++ scripts/make-pipeline.pl
@@ -938,7 +938,7 @@ if(defined($ref_path))
         for my $assem (qw(links plain)) {
           my $callroot = "$call.$pop.$assem.$kmerstr";
           print "$proj/k$k/vcfcov/$callroot.%.vcf.gz: $proj/vcfs/$callroot.vcf.gz $proj/k$k/graphs/%.raw.ctx | dirs\n";
-          print "\t$mccortex vcfcov -m \$(MEM) \$(VCFCOV_ARGS) --ref $ref_path --out-fmt vcfgz --out \$@ \$^ >& \$@.log\n\n";
+          print "\t$mccortex vcfcov -m \$(MEM) \$(VCFCOV_ARGS) --ref $ref_path --out-fmt vcfgz \$^ | \$(BCFTOOLS) view --samples \"\$*\" -o \$@ -O z >& \$@.log\n\n";
         }
       }
     }
