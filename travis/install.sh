6515627790fdd45dea2173e3788188b34bce501b
Isaac Turner <turner.isaac@gmail.com>
2016-11-02 23:12:47 +0000

    Travis CI: install.sh fix bash syntax in prev commit

--- travis/install.sh
+++ travis/install.sh
@@ -5,7 +5,7 @@ set -e
 # Only run if we are not doing Coverity Scan analysis
 # The COVERITY_SCAN_BRANCH environment variable is not set until AFTER install
 # step has run, so we do a check on which git branch we have
-if[ $(git rev-parse --abbrev-ref HEAD) != "coverity_scan" ]
+if [ $(git rev-parse --abbrev-ref HEAD) != "coverity_scan" ]
 then
 
   # Set up cpanm, install JSON perl package
